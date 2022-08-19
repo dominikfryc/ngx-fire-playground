5ac3dc25ac1a22c2142747d32fecd7085cf21427
Dominik Fryč <fryc.dominik@gmail.com>
2022-08-19 02:04:22 +0200

    Home component eagerly loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HoverPreloadModule, HoverPreloadStrategy } from 'ngx-hover-preload';
+import { HomeComponent } from './components/home/home.component';
 import { UserResolver } from './resolvers/user.resolver';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    pathMatch: 'full',
+    component: HomeComponent,
   },
   {
     path: 'resolver',
